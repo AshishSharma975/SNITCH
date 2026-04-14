377e163ceb51be3edbf98f09f43307bcdf3d960a
AshishSharma975 <ashish8389sharma@gmail.com>
2026-04-14 15:26:16 +0530

    feat: implement product creation route with validation and image upload middleware

--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { authenticateSeller } from "../middlewares/auth.middleware.js";
 import { createProduct } from "../controllers/product.controller.js";
 import multer from "multer";
+import { createProductValidator } from "../validator/product.validator.js";
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -15,8 +16,10 @@ const router = express.Router();
 router.post(
   "/create",
   authenticateSeller,
+  createProductValidator,
   upload.array("images", 7), 
   createProduct              
 );
+
 
 export default router;
